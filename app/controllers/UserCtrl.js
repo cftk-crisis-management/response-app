8b97ebf323c17bf8e1a330ce242b16b7eb4239db
Nick Nash <nicknash@Nicks-MBP-2.lipscomb.edu>
2017-03-11 14:17:41 -0600

    Edited userCtrl

--- app/controllers/UserCtrl.js
+++ app/controllers/UserCtrl.js
@@ -1,5 +1,29 @@
 "use strict";
 
-app.controller( function() {
+app.controller( "UserCtrl", function($scope, $window, AuthFactory) {
+
+	$scope.loginGoogle = function () {
+		console.log("you clicked login with Google");
+		AuthFactory.authWithProvider()
+		.then(function(result) {
+	    	var user = result.user.uid;
+	    	console.log("logged in user:", user);
+	    	//Once logged in, go to another view
+	    	// $location.path("/login-details");
+	    	$scope.$apply();
+	  	}).catch(function(error) {
+	    	// Handle the Errors.
+	    	console.log("error with google login", error);
+	    	var errorCode = error.code;
+	    	var errorMessage = error.message;
+	    	// The email of the user's account used.
+	    	var email = error.email;
+	    	// The firebase.auth.AuthCredential type that was used.
+	    	var credential = error.credential;
+	    	// ...
+	  	});
+	};
+
+
 
 });
