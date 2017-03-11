20d6e55756e6925dc667fb8d712587cd8a11ba8a
Harper Frankstone <Harper Frankstone>
2017-03-11 17:01:51 -0600

    pushing up the getTweets function

--- app/factories/twitterFactory.js
+++ app/factories/twitterFactory.js
@@ -4,10 +4,10 @@ app.factory("TwitterFactory", function($http, $q, GeoFireFactory){
 	console.log("TwitterFactory");
 	
 	//supposed to get this info from the GeoFireFactory.js
-	 let userLat = GeoFireFactory.geolocationCallback();
+	 let userLat = GeoFireFactory.geolocationCallback;
 	 let userLong = GeoFireFactory.longitude;
 	 let userRadius = GeoFireFactory.radius;
-	 let getUserLocation = () => {
+	 let  = () => {
 	 	userLat;
 	 		console.log("latitude", userLat);
 		userLong;
