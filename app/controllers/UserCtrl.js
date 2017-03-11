3017bc64949b6e8c0acbd83460305a896944ff6d
Harper Frankstone <Harper Frankstone>
2017-03-11 14:17:52 -0600

    working on the UserCtrl

--- app/controllers/UserCtrl.js
+++ app/controllers/UserCtrl.js
@@ -1,5 +1,27 @@
 "use strict";
 
-app.controller( function() {
+app.controller( function($scope, $location, AuthFactory) {
 
+let loginGoogle = function () {
+		console.log("you clicked login with Google");
+		AuthFactory.authWithProvider()
+		.then(function(result) {
+	    	var user = result.user.uid;
+	    	console.log("logged in user:", user);
+	    	//Once logged in, go to another view
+	    	// $location.path("/login-details");
+	    	$scope.$apply();
+	  	}).catch(function(error) {
+	    	// Handle the Errors.
+	    	console.log("error with google login", error);
+	    	var errorCode = error.code;
+	    	var errorMessage = error.message;
+	    	// The email of the user's account used.
+	    	var email = error.email;
+	    	// The firebase.auth.AuthCredential type that was used.
+	    	var credential = error.credential;
+	    	// ...
+	  	});
+	};
+	
 });
