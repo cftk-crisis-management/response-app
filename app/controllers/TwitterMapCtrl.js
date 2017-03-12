66924102757223bfefb43e1339e9d4e496eb3f65
Nick Nash <nicknash@Nicks-MBP-2.lipscomb.edu>
2017-03-11 20:28:49 -0600

    Nick's twitter update

--- app/controllers/TwitterMapCtrl.js
+++ app/controllers/TwitterMapCtrl.js
@@ -9,25 +9,7 @@ console.log("TwitterMapCtrl");
 
 	TwitterFactory.getUserLocation();
 
-	let getTweets = () => {
-		let tweets = [];
-		let user = AuthFactory.getUser();
 
-		return $q((resolve, reject) => {
-			$http.get(`https://api.twitter.com/1.1/geo/search.json?query=${user.latitude},${user.longitude}`)
-			.then((tweetObject) => {
-				let tweetCollection = tweetObject.data;
-				Object.keys(tweetCollection).forEach((key) => {
-					tweetCollection[key].id = key;
-					tweets.push(tweetCollection[key]);
-				});
-				resolve(tweets);
-			})
-			.catch((error) => {
-				reject(error);
-			});
-		});
-	};
 
 
 });
