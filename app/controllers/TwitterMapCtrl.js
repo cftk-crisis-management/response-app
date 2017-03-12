5f41116c8cf57b3ed7fd7cc67f159f2e1d999437
Dean Smith <Dean Smith>
2017-03-12 00:17:07 -0600

    Adding progress I made on google freaking mapsgit add .

--- app/controllers/TwitterMapCtrl.js
+++ app/controllers/TwitterMapCtrl.js
@@ -5,11 +5,17 @@ console.log("TwitterMapCtrl");
 
 //we call on the GeoFireFactory to get the user's location, then we send that location to the Twitterfactory.
 //Then the TwitterFactory will use that user's location to query the twitter api
-	GeoFireFactory.getLocation();
+	var map;
+  function initMap() {
+    console.log("DO I EXIST!!!");
+    map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: -34.397, lng: 150.644},
+       zoom: 8
+    });
+  }
+  initMap();
+ // GeoFireFactory.getLocation();
 
-	TwitterFactory.getUserLocation();
-
-
-
-
+  TwitterFactory.getUserLocation();
 });
+
