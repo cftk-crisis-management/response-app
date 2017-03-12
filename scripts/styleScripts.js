da2a2b38ad85f3241a00c3a428804371c9c958df
webbdm <gwebb762@gmail.com>
2017-03-11 22:28:13 -0600

    added image jQuery functions

---
+++ scripts/styleScripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#fireIcon').click(function(){
+        $('#floodIcon').attr("src","../images/FLOOD GRAYED.png");
+        $('#accidentIcon').attr("src","../images/ACCIDENT_GRAY.png");
+    });
+});
